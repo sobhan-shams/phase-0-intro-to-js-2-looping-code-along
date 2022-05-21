6a0d433a29e414fe542447704eeb6c8ebdb4886a
sobhan <somayahshams1995@gmail.com>
2022-05-21 07:33:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+
+    const messages = [];
+   
+     for (let i = 0; i < names.length; i++) {
+       const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+       messages.push(message);}
+
+     return messages;
+   }
+   
+   console.log(writeCards(['Bob', 'Sue'], 'wedding'));
+   
+   function countDown(fromNumber) {
+    console.log(fromNumber);
+
+    let nextNumber = fromNumber - 1;
+
+    if (nextNumber >= 0) {
+        countDown(nextNumber);
+    }
+}
+countDown(3);
